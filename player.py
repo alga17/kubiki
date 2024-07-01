e2a3d633579dec7d38d4046f4bd6fb21f22af033
alga17 <derrorb@gmail.com>
2024-07-01 09:35:09 +0300

    1

---
+++ player.py
@@ -0,0 +1,16 @@
+class Cube(object):
+  def __init__(self, up=None):
+    self.up = up
+
+  def roll(self):
+    sides = ['tank', 'deathray', 'deathray', 'human', 'cow', 'chicken']
+    self.up = random.choice(sides)
+
+  def __repr__(self):
+    return str(self.up)
+
+  def __str__(self):
+    return self.up
+
+  def __eq__(self, other):
+    return self.up == other.up
