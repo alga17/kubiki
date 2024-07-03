3d0341040d4f338efb3940395faea3c40323bde5
alga17 <derrorb@gmail.com>
2024-07-03 09:59:09 +0300

    1

--- player.py
+++ player.py
@@ -5,7 +5,7 @@ class Cube(object):
 
   def roll(self):
     sides = ['tank', 'deathray', 'deathray', 'human', 'cow', 'chicken']
-    self.up = random.choice(sides)
+    self.side = sides
 
   def __repr__(self):
     return str(self.up)
@@ -15,3 +15,39 @@ class Cube(object):
 
   def __eq__(self, other):
     return self.up == other.up
+
+class Player(object):
+  def __init__(self,name):
+    self.name = name
+    self.score = 0
+
+  def pick(self):
+    choicedict = {'k': 'chicken', 'c': 'cow', 'h': 'human', 'd': 'deathray', 's': 'stop'}
+    if not self.dice:
+      return
+    valid_choice = False
+    while not valid_choice:
+      selected = raw_input('Choice? k, c, d, h, s')
+      if selected == 's':
+        self.dice = []
+        return
+      elif selected not in choicedict.keys():
+        print('Неверный выбор!')
+      elif choicedict[selected] not in [str(x) for x in self.dice]:
+        print('Нет %s для выбора' % choicedict[selected])
+      elif selected in ['k', 'c', 'h']:
+
+        if choicedict[selected] in [str(x) for x in self.saved]:
+          print("Вы уже сохранили" % choicedict[selected])
+        else:
+          for index, d in enumerate(self.dice):
+            if d.up == choicedict[selected]:
+              self.saved.append(d)
+          self.dice[:] = (value for value in self.dice if value.up != choicedict[selected])
+          valid_choice = True
+      elif selected == 'd':
+        for index, d in enumerate(self.dice):
+          if d.up == 'deathray':
+            self.saved.append(d)
+        self.dice[:] = (value for value in self.dice if value.up != 'deathray')
+        valid_choice = True
--- test.py
+++ test.py
@@ -1,10 +1,17 @@
-import unittest
+import json
+from player import Cube
+def test_init():
+    a = Cube()
+    assert a.side in ['tank', 'deathray', 'deathray', 'human', 'cow', 'chicken']
+    b = Cube('tank')
+    assert b.side == 'tank'
 
+def test_repr():
+    c = Cube()
+    assert type(c) == Cube
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+def test_load():
+    x = Cube('tank')
+    x = x.load('tank')
+    assert x.side == 'tank'
 
-
-if __name__ == '__main__':
-    unittest.main()
